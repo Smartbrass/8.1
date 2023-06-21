4fff9bb9dbe8be23df7bf89352f47a8672493426
Smartbrass <136761121+Smartbrass@users.noreply.github.com>
2023-06-21 21:46:12 +0300

    Update _8.1.py

--- _8.1.py
+++ _8.1.py
@@ -1,7 +1,13 @@
-N = int(input())
+N = int(input('Введите число от 1 до 10 000:'))
 N1 = []
-for i in range(N):
-   a = int(input())
-   N1.append(a)
-N1.reverse()
-print(N1)
+if N < 1 or N > 10000:
+    print("Не правильно!")
+else:
+    for i in range(N):
+       a = int(input('Введите число по модулю не более 10е5:'))
+       if a > 10e5:
+           print("Не правильно!")
+       else:
+            N1.append(a)
+    N1.reverse()
+    print(N1)
